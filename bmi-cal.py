393d4c53e63fa1d6e745fa7b454adce0976e9b01
Nassir ∞ <118899988+soynassir@users.noreply.github.com>
2023-01-30 11:36:53 +0000

    Create bmi-cal.py

---
+++ bmi-cal.py
@@ -0,0 +1,11 @@
+# BMI = weight ÷ height^2
+# Weight is measured in kilograms
+# Height is measured in metres.
+
+weight = float(input("Enter your weight: "))
+
+height = float(input("Enter your height: "))
+
+bmi = round(weight / (height**2),2) 
+
+print("Your BMI is " + str(bmi))
