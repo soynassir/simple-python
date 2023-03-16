2fec4b815966d4d54be0232ab3b455d49939bf0f
Nassir ∞ <118899988+soynassir@users.noreply.github.com>
2023-03-16 11:02:19 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# Created a variable to store the user's name
+user_name = input("What is your name? ")
+
+# Display the user's name 
+print(user_name)
+
+# Created a variable to store the user's age
+user_age = input("How old are you? ")
+
+# Display the user's age 
+print(user_age)
+
+# Display Hello World! message
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,18 @@
+# Ask the user for their 3 favourite food items on a menu
+
+# Store the user's first favourite food on item1 
+item1 = input("Type your first favourite food: ")
+
+# Store the user's second favourite food on item2
+item2 = input("Type your second favourite food: ")
+
+# Store the user's third favourite food on item3
+item3 = input("Type your third favourite food: ")
+
+# Display a confirmation message of the user's order
+print("Order confirmation! You have ordered:")
+
+# Display the collected favourite food from the user
+print(item1)
+print(item2)
+print(item3)
