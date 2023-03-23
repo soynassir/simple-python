ff5243d3dd42d77fd0694ac164f26d3e87ed21d7
Nassir ∞ <118899988+soynassir@users.noreply.github.com>
2023-03-23 12:32:22 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,21 +8,24 @@ operation = input("Enter an operation(+,-,/,*): ")
 users_history = 'calculator_data.txt'
 
 # Save the user input and the answer to the txt file
-with open(users_history, 'w') as data_file:
+with open(users_history, 'a') as data_file:
     if operation == "+":
         answer = number1 + number2
         data_file.write("{} + {} = {}\n".format(number1, number2, answer))
     elif operation == "*":
         answer = number1 * number2
-        data_file.write("{} * {} = {}".format(number1, number2, answer))
+        data_file.write("{} * {} = {}\n".format(number1, number2, answer))
     elif operation == "/":
         answer = number1 / number2
-        data_file.write("{} / {} = {}".format(number1, number2, answer))
+        data_file.write("{} / {} = {}\n".format(number1, number2, answer))
     elif operation == "-":
         answer = number1 - number2
-        data_file.write("{} - {} = {}".format(number1, number2, answer))
+        data_file.write("{} - {} = {}\n".format(number1, number2, answer))
     else:
         print("You haven't entered the right operator!")
 
 read_data = open(users_history, 'r')
 print(read_data.read())
+read_data.close()
+
+
