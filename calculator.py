9052bbd6eb62e451ef084d56463825068580bef1
Nassir ∞ <118899988+soynassir@users.noreply.github.com>
2023-03-23 14:01:31 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,31 +1,46 @@
 ''' This program is a simple calculator that will ask user to enter
     two numbers and an operation(+,-,/,*) '''
-number1 = int(input("Enter a first number: "))
-number2 = int(input("Enter a second number: "))
 
-operation = input("Enter an operation(+,-,/,*): ")
+while True:
+    try:
+        number1 = int(input("Enter a first number: "))
+        number2 = int(input("Enter a second number: "))
+
+        operation = input("Enter an operation(+,-,/,*): ")
+        break
+    except ValueError as typo:
+        print("That wasn't a number, try again!\n")
+        print(typo)
 
 users_history = 'calculator_data.txt'
 
 # Save the user input and the answer to the txt file
 with open(users_history, 'a') as data_file:
+    
     if operation == "+":
         answer = number1 + number2
-        data_file.write("{} + {} = {}\n".format(number1, number2, answer))
+        data_file.write("{} + {} = {}\n".format(number1, number2, answer))        
+        print("Answer is ", answer)
+
     elif operation == "*":
         answer = number1 * number2
         data_file.write("{} * {} = {}\n".format(number1, number2, answer))
+        print("Answer is ", answer)
+
     elif operation == "/":
         answer = number1 / number2
         data_file.write("{} / {} = {}\n".format(number1, number2, answer))
+        print("Answer is ", answer)
+
     elif operation == "-":
         answer = number1 - number2
         data_file.write("{} - {} = {}\n".format(number1, number2, answer))
+        print("Answer is ", answer)
+
     else:
         print("You haven't entered the right operator!")
 
-read_data = open(users_history, 'r')
-print(read_data.read())
-read_data.close()
+# read_data = open(users_history, 'r')
+# print(read_data.read())
 
 
